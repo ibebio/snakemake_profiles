6b9454972b8c8fa061b9bdada0c21f1049c91e50
Ilja Bezrukov <ibezrukov@tuebingen.mpg.de>
2020-08-11 12:51:30 +0200

    Remove conda path vodoo -- not required (anymore)
    Snakemake is now apparently clever enough to harvest the environmental variables from the submit host where it is running and set them on the cluster nodes before it activates the conda environment. Thus, the path setting before disrupted finding the right python version inside the environment.

--- sge/sge-jobscript.sh
+++ sge/sge-jobscript.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # properties = {properties}
-if [[ -f ~/.bashrc &&  $(grep -c "__conda_setup=" ~/.bashrc) -gt 0 && $(grep -c "unset __conda_setup" ~/.bashrc) -gt 0 ]]; then
-   echo "Sourcing .bashrc" 1>&2
-   . ~/.bashrc
-else
-   echo "Exporting conda PATH" 1>&2
-   export PATH=/ebio/abt6_projects9/abt6_software/bin/miniconda3/bin/:$PATH
-fi
 
 {exec_job}
